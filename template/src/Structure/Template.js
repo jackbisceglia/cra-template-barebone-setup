012cd01739dc8bcb4011995f5c0495989e0d4e0c
jackbisceglia <jackbisceglia2000@gmail.com>
2021-01-19 15:41:26 -0500

    files added

--- template/src/Structure/Template.js
+++ template/src/Structure/Template.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 export default function App() {
     return (
         <div className="wrapper">
-            <main>
-                <h1>CRA-Barebones-Template</h1>
+            <main style={{padding: '5rem 0', display: 'flex', justifyContent: 'center'}}>
+                <h1 style={{margin: '0', color: 'white', fontSize: '3.5rem'}} >CRA-Barebones-Template</h1>
             </main>
         </div>
     )
